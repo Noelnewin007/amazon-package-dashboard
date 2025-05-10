fbfd035afa5bd1537b0b633704d8aba27b4d9e75
Noel <noelnewin007@gmail.com>
2025-05-10 12:52:00 -0400

    Set port to 5000 for Railway deploy

--- server.py
+++ server.py
@@ -62,4 +62,4 @@ if __name__ == "__main__":
     # Get the port from the environment variable, or default to 5000
     port = int(os.environ.get("PORT", 5000))
     # Run the app in production-ready mode (change the host to allow external access if needed)
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
